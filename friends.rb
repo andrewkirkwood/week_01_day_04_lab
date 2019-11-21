1b5ccab55b3a383f4cda957e8dcf70878b0a61a7
Andrew Kirkwood <andrewkirkwood90@gmail.com>
2019-11-21 15:02:44 +0000

    completed test 2

--- friends.rb
+++ friends.rb
@@ -1,3 +1,7 @@
 def get_name(person)
   return person[:name]
 end
+
+def getting_favourite_tv_show(person)
+  return person[:favourites][:tv_show]
+end
--- specs/friends_spec.rb
+++ specs/friends_spec.rb
@@ -76,9 +76,19 @@ class TestFriends < MiniTest::Test
   # 2. For a given person, return their favourite tv show
   # (e.g. the function favourite_tv_show(@person2) should return the string "Baywatch")
 
+def test_getting_favourite_tv_show
+  expected = "Baywatch"
+  actual = getting_favourite_tv_show(@person2)
+  assert_equal(expected , actual)
+end
 
   # 3. For a given person, check if they like a particular food
   # (e.g. the function likes_to_eat(@person2, "bread") should return true, likes_to_eat(@person3, "spinach") should return false)
+
+  def test_check_if_person_likes_a_food
+    expected = ""
+    actual = ""
+  end
 
   # 4. For a given person, add a new name to their list of friends
   # (e.g. the function add_friend(@person2, "Scrappy-Doo") should add Scrappy-Doo to the friends.)
